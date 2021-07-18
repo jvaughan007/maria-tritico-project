aea566b2e416b1d54ab876a82cf887603a13c5a2
Joshua Vaughan <jvaughan007@yahoo.com>
2021-07-18 00:56:33 -0500

    finished NavBar structure, needs styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,30 @@
-import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const NavBar = () => {
 
     return (
-        <StyledNav>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/home">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/marias-story">Maria's Story</Link>
-                    </li>
-                    <li>
-                        <Link to="/causes">Causes</Link>
-                    </li>
-                    <li>
-                        <Link to="/donate">Donate</Link>
-                    </li>
-                </ul>
-            </div>
-        </StyledNav>
+        <>
+            <StyledNav>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/marias-story">Maria's Story</Link>
+                        </li>
+                        <li>
+                            <Link to="/causes">Causes</Link>
+                        </li>
+                        <li>
+                            <Link to="/donate">Donate</Link>
+                        </li>
+                    </ul>
+                </div>
+            </StyledNav>
+        </>
     );
 };
 
